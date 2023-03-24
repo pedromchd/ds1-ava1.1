d9b52824c7242571d1d3d580f1e918d0c30cab64
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-03-24 17:59:11 -0300

    Page Design

--- index.php
+++ index.php
@@ -9,11 +9,31 @@
   <link rel="stylesheet" href="css/styles.css">
 </head>
 
-<body>
+<body class="mx-24 flex flex-col">
   <img id="dvd" src="img/dvd.png" alt="">
-  <header class="py-5">
-    <h1 class="font-2p text-7xl text-center text-white">QWERTY GAMES</h1>
+  <header class="py-6 flex items-center justify-around">
+    <img src="img/sword.gif" alt="">
+    <h1 class="font-2p text-7xl text-white">QWERTY GAMES</h1>
+    <img src="img/torch.gif" alt="">
   </header>
+  <main class="flex-grow grid grid-cols-2">
+    <section class="flex items-stretch justify-center">
+      <article class="my-7 w-96 w98-container">
+        <div class="w98-title-bar flex items-center justify-between">
+          <h2 class="text-white font-mono">Login to Querty Games</h2>
+          <div class="w98-container">
+            <svg width="18" height="18" viewBox="0 0 100 100">
+              <line stroke-width="10" stroke="#111" stroke-linecap="round" x1="25" y1="25" x2="75" y2="75" />
+              <line stroke-width="10" stroke="#111" stroke-linecap="round" x1="75" y1="25" x2="25" y2="75" />
+            </svg>
+          </div>
+        </div>
+      </article>
+    </section>
+    <aside class="grid place-items-center">
+      <img src="img/tv.gif" alt="">
+    </aside>
+  </main>
 </body>
 
 </html>
