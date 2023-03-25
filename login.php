735ccfe19c2993bf3ad52156a7715eaed00b5ef2
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-03-24 22:00:49 -0300

    Website Polishment

--- login.php
+++ login.php
@@ -5,34 +5,23 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Querty Library</title>
   <link rel="stylesheet" href="css/styles.css">
 </head>
 
-<body class="mx-24 flex flex-col">
-  <img id="dvd" src="img/dvd.png" alt="">
-  <header class="py-6 flex items-center justify-around">
-    <img src="img/sword.gif" alt="">
-    <h1 class="font-2p text-7xl text-white">QWERTY GAMES</h1>
-    <img src="img/torch.gif" alt="">
-  </header>
-  <main class="flex-grow grid grid-cols-2">
-    <section class="flex items-stretch justify-center">
-      <article class="my-7 w-96 w98-container">
-        <div class="w98-title-bar flex items-center justify-between">
-          <h2 class="text-white font-mono">Login to Querty Games</h2>
-          <div class="w98-container">
-            <svg width="18" height="18" viewBox="0 0 100 100">
-              <line stroke-width="10" stroke="#111" stroke-linecap="round" x1="25" y1="25" x2="75" y2="75" />
-              <line stroke-width="10" stroke="#111" stroke-linecap="round" x1="75" y1="25" x2="25" y2="75" />
-            </svg>
-          </div>
+<body class="grid grid-cols-3 place-items-center">
+  <main class="col-span-2">
+    <section class="w-96 h-96 win98-container shadow-sm shadow-black">
+      <div class="win98-title-bar flex items-center justify-between">
+        <h1 class="font-mono text-white">Welcome to Querty Library</h1>
+        <div class="win98-container">
+          <svg width="10" height="10" viewBox="0 0 100 100">
+            <line stroke-width="20" stroke="#000" stroke-linecap="round" x1="10" y1="10" x2="90" y2="90" />
+            <line stroke-width="20" stroke="#000" stroke-linecap="round" x1="90" y1="10" x2="10" y2="90" />
+          </svg>
         </div>
-      </article>
+      </div>
     </section>
-    <aside class="grid place-items-center">
-      <img src="img/tv.gif" alt="">
-    </aside>
   </main>
 </body>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,27 +3,9 @@ module.exports = {
   content: ["./**/*.php"],
   theme: {
     extend: {
-      animation: {
-        dvd: 'hue 7.75s linear infinite, moveX 2.25s linear infinite alternate, moveY 3.75s linear infinite alternate'
-      },
-      keyframes: {
-        hue: {
-          'to': { filter: 'hue-rotate(360deg)' }
-        },
-        moveX: {
-          'from': { left: 0 },
-          'to': { left: 'calc(100vw - var(--dvd-width))' }
-        },
-        moveY: {
-          'from': { top: 0 },
-          'to': { top: 'calc(100vh - var(--dvd-heigth))' }
-        }
-      },
       fontFamily: {
-        'cinzel': ['"Cinzel Decorative"', 'cursive'],
-        'monoton': ['"Monoton"', 'cursive'],
-        '2p': ['"Press Start 2P"', 'cursive']
-      }
+        '2p': ['"Press Start 2P"', 'cursive'],
+      },
     },
   },
   plugins: [],
