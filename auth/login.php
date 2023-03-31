e57179c40b8d72dff8da578344335eb5758d1917
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-03-31 09:36:15 -0300

    X close box

--- auth/login.php
+++ auth/login.php
@@ -14,8 +14,14 @@
   <main class="h-screen grid grid-cols-3 place-items-center">
     <div class="col-span-2 border-2 border-[#fff_#999_#999_#fff] outline-1 [outline-style:outset]">
       <section class="p-0.5 min-w-[31rem] bg-[#ccc]">
-        <header class="px-1 bg-gradient-to-r from-[#009] to-[#09f]">
-          <h1 class="font-mono text-white">Querty Library Login</h1>
+        <header class="px-1 bg-gradient-to-r from-[#009] to-[#09f] flex items-center justify-between">
+          <h1 class="font-mono text-white">Register New Account</h1>
+          <div class="p-0.5 bg-[#ccc] col-span-2 border-2 border-[#fff_#999_#999_#fff] outline-1 [outline-style:outset]">
+            <svg width="10" height="10" viewBox="0 0 100 100">
+              <line stroke-width="20" stroke="#000" stroke-linecap="round" x1="10" y1="10" x2="90" y2="90" />
+              <line stroke-width="20" stroke="#000" stroke-linecap="round" x1="90" y1="10" x2="10" y2="90" />
+            </svg>
+          </div>
         </header>
         <div class="p-4 space-x-5 flex justify-between">
           <div><img src="/img/keys.png" alt=""></div>
