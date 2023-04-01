2dde2729421fba298ec0f3cac063aabdd6167016
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-04-01 11:58:51 -0300

    Homepage partial

--- home/index.php
+++ home/index.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,19 +9,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login - Querty Library</title>
   <link rel="stylesheet" href="/css/styles.css">
-  <link rel="icon" href="/img/joystick.png" type="image/x-icon">
+  <link rel="icon" href="/img/joystick-32x32.png" type="image/x-icon">
 </head>
 
 <body class="bg-[url(/img/zeal.png)] bg-cover bg-right-bottom">
-  <main class="h-screen grid grid-cols-3 place-items-center">
-    <div class="col-span-2 border-2 border-[#fff_#999_#999_#fff] outline-1 [outline-style:outset]">
+  <main class="h-screen grid grid-cols-2 grid-rows-3 place-items-center">
+    <div class="border-2 [border-style:outset] outline-1 [outline-style:outset]">
       <section class="p-0.5 min-w-[31rem] bg-[#ccc]">
         <header class="px-1 bg-gradient-to-r from-[#009] to-[#09f] flex items-center justify-between">
           <h1 class="font-mono text-white">Querty Library</h1>
-          <div class="p-0.5 bg-[#ccc] col-span-2 border-2 border-[#fff_#999_#999_#fff] outline-1 [outline-style:outset]">
+          <div class="p-0.5 bg-[#ccc] border-2 [border-style:outset] outline-1 [outline-style:outset]">
             <svg width="10" height="10" viewBox="0 0 100 100">
-              <line stroke-width="20" stroke="#000" stroke-linecap="round" x1="10" y1="10" x2="90" y2="90" />
-              <line stroke-width="20" stroke="#000" stroke-linecap="round" x1="90" y1="10" x2="10" y2="90" />
+              <path d="M 10 10 L 90 90" stroke="#000" stroke-width="20" stroke-linecap="round" />
+              <path d="M 90 10 L 10 90" stroke="#000" stroke-width="20" stroke-linecap="round" />
             </svg>
           </div>
         </header>
@@ -29,13 +31,50 @@
             <p>Welcome to Querty Library!</p>
             <p>Add, edit or delete your game records.</p>
           </article>
-          <div class="space-y-3">
+          <div class="space-y-3 font-mono">
             <a href="/auth/logout.php" class="block">
-              <button class="w-20 border-2 border-[#fff_#999_#999_#fff] outline-1 [outline-style:outset]">
+              <button class="w-20 bg-[#ccc] border-2 [border-style:outset] outline-1 [outline-style:outset]">
                 Logout
               </button>
             </a>
           </div>
+        </div>
+      </section>
+    </div>
+    <div class="row-start-2 col-span-2 row-span-2 border-2 [border-style:outset] outline-1 [outline-style:outset]">
+      <section class="p-0.5 min-w-[74rem] max-w-[74rem] min-h-[25rem] max-h-[25rem] bg-[#ccc] flex flex-col">
+        <header class="px-1 bg-gradient-to-r from-[#009] to-[#09f] flex items-center justify-between">
+          <div><img src="/img/joystick-16x16.png" alt=""></div>
+          <h1 class="ml-1 font-mono text-white flex-grow"><?="$_SESSION[username]'s Library"?></h1>
+          <div class="p-0.5 bg-[#ccc] border-2 [border-style:outset] outline-1 [outline-style:outset]">
+            <svg width="10" height="10" viewBox="0 0 100 100">
+              <path d="M 10 10 L 90 90" stroke="#000" stroke-width="20" stroke-linecap="round" />
+              <path d="M 90 10 L 10 90" stroke="#000" stroke-width="20" stroke-linecap="round" />
+            </svg>
+          </div>
+        </header>
+        <div class="p-[1px] my-1 font-mono">
+          <button class="bg-[#ccc] border-2 [border-style:outset] outline-1 [outline-style:outset]">
+            <div class="w-32 py-1 px-2 flex items-center space-x-3">
+              <img src="/img/devices-32x32.png" alt="">
+              <p>Add new</p>
+            </div>
+          </button>
+          <button class="bg-[#ccc] border-2 [border-style:outset] outline-1 [outline-style:outset]">
+            <div class="w-32 py-1 px-2 flex items-center space-x-3">
+              <img src="/img/write-32x32.png" alt="">
+              <p>Edit</p>
+            </div>
+          </button>
+          <button class="bg-[#ccc] border-2 [border-style:outset] outline-1 [outline-style:outset]">
+            <div class="w-32 py-1 px-2 flex items-center space-x-3">
+              <img src="/img/bin-32x32.png" alt="">
+              <p>Delete</p>
+            </div>
+          </button>
+        </div>
+        <div class="bg-[#fff] flex-grow border-2 [border-style:inset] overflow-y-scroll">
+
         </div>
       </section>
     </div>
