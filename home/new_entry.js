1d4665385ab280546e452c1e4c1d2aee1c0205fb
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-04-02 15:52:03 -0300

    New entry page and homepage table

---
+++ home/new_entry.js
@@ -0,0 +1,13 @@
+const form = document.newEntry.cover;
+const thumb = document.querySelector('#thumbnail');
+
+form.addEventListener('change', handleFile);
+
+function handleFile() {
+  const file = this.files[0];
+  const reader = new FileReader();
+  reader.onload = (evt) => {
+    thumb.style.background = `#fff url(${evt.target.result}) no-repeat center / contain`;
+  }
+  reader.readAsDataURL(file);
+}
--- home/new_entry.php
+++ home/new_entry.php
@@ -2,13 +2,6 @@
 session_start();
 
 $uname = $_SESSION['username'];
-
-$db = new mysqli('localhost', 'root', '', 'library');
-$stmt = $db->prepare('SELECT game.* FROM game JOIN user ON game.user = user.id WHERE user.name = ?');
-$stmt->bind_param('s', $uname);
-$stmt->execute();
-
-$result = $stmt->get_result();
 ?>
 
 <!DOCTYPE html>
@@ -18,9 +11,10 @@ $result = $stmt->get_result();
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login - Querty Library</title>
+  <title>Insert new - Querty Library</title>
   <link rel="stylesheet" href="/css/styles.css">
   <link rel="icon" href="/img/joystick-32x32.png" type="image/x-icon">
+  <script src="new_entry.js" defer></script>
 </head>
 
 <body class="bg-[url(/img/zeal.png)] bg-cover bg-right-bottom">
@@ -52,11 +46,11 @@ $result = $stmt->get_result();
         </div>
       </section>
     </div>
-    <div class="row-start-2 col-span-3 row-span-2 border-2 [border-style:outset] outline-1 [outline-style:outset]">
-      <section class="p-0.5 min-w-[75rem] max-w-[75rem] min-h-[25rem] max-h-[25rem] bg-[#ccc] flex flex-col">
+    <div class="row-start-2 col-span-2 row-span-2 border-2 [border-style:outset] outline-1 [outline-style:outset]">
+      <section class="p-0.5 min-w-[31rem] bg-[#ccc]">
         <header class="px-1 bg-gradient-to-r from-[#009] to-[#09f] flex items-center justify-between">
-          <div><img src="/img/joystick-16x16.png" alt=""></div>
-          <h1 class="ml-1 font-mono text-white flex-grow"><?= "$uname's Library" ?></h1>
+          <div><img src="/img/devices-16x16.png" alt=""></div>
+          <h1 class="ml-1 font-mono text-white flex-grow">Add new entry</h1>
           <div class="p-0.5 bg-[#ccc] border-2 [border-style:outset] outline-1 [outline-style:outset]">
             <svg width="10" height="10" viewBox="0 0 100 100">
               <path d="M 10 10 L 90 90" stroke="#000" stroke-width="20" stroke-linecap="round" />
@@ -64,47 +58,46 @@ $result = $stmt->get_result();
             </svg>
           </div>
         </header>
-        <div class="p-[1px] my-1 font-mono">
-          <a href="new_entry.php">
-            <button class="bg-[#ccc] border-2 [border-style:outset] outline-1 [outline-style:outset]">
-              <div class="w-32 py-1 px-2 flex items-center space-x-3">
-                <img src="/img/devices-32x32.png" alt="">
-                <p>Add new</p>
+        <div class="p-4 space-x-5 flex justify-between">
+          <article class="flex-grow">
+            <p>Please fill in this form to add your game data.</p>
+            <form name="newEntry" id="newEntry" action="insert_data.php" method="post" class="mt-3 space-y-3">
+              <div class="flex justify-between">
+                <label for="name">Name:</label>
+                <input type="text" name="name" id="name" class="w-[70%] px-1 outline-1 [outline-style:inset]">
               </div>
-            </button>
-          </a>
-          <button class="bg-[#ccc] border-2 [border-style:outset] outline-1 [outline-style:outset]">
-            <div class="w-32 py-1 px-2 flex items-center space-x-3">
-              <img src="/img/write-32x32.png" alt="">
-              <p>Edit</p>
+              <div class="flex justify-between">
+                <label for="year">Release year:</label>
+                <input type="number" name="year" id="year" class="w-[70%] px-1 outline-1 [outline-style:inset]">
+              </div>
+              <div class="flex justify-between">
+                <label for="system">System:</label>
+                <input type="text" name="system" id="system" class="w-[70%] px-1 outline-1 [outline-style:inset]">
+              </div>
+              <div class="flex justify-between">
+                <label for="developer">Developer:</label>
+                <input type="text" name="developer" id="developer" class="w-[70%] px-1 outline-1 [outline-style:inset]">
+              </div>
+              <div class="flex justify-between">
+                <label for="cover">Cover:</label>
+                <input type="file" name="cover" id="cover" class="w-[70%]">
+              </div>
+            </form>
+          </article>
+          <div class="space-y-3 font-mono flex flex-col">
+            <p>Preview:</p>
+            <div id="thumbnail" class="flex-grow border border-dashed border-black bg-[#fff]"></div>
+            <div>
+              <button form="newEntry" class=" w-20 bg-[#ccc] border-2 [border-style:outset] outline-1 [outline-style:outset]">
+                Submit
+              </button>
+              <a href="index.php" class="">
+                <button class="w-20 bg-[#ccc] border-2 [border-style:outset] outline-1 [outline-style:outset]">
+                  Cancel
+                </button>
+              </a>
             </div>
-          </button>
-          <button class="bg-[#ccc] border-2 [border-style:outset] outline-1 [outline-style:outset]">
-            <div class="w-32 py-1 px-2 flex items-center space-x-3">
-              <img src="/img/bin-32x32.png" alt="">
-              <p>Delete</p>
-            </div>
-          </button>
-        </div>
-        <div class="bg-[#fff] flex-grow border-2 [border-style:inset] overflow-y-scroll">
-          <table class="w-[100%]">
-            <tr>
-              <th>Name</th>
-              <th>System (Console)</th>
-              <th>Release Year</th>
-              <th>Developer</th>
-              <th>Cover</th>
-            </tr>
-            <?php while ($row = $result->fetch_assoc()) : ?>
-              <tr>
-                <td><?= $row['cover'] ?></td>
-                <td><?= $row['name'] ?></td>
-                <td><?= $row['year'] ?></td>
-                <td><?= $row['system'] ?></td>
-                <td><?= $row['developer'] ?></td>
-              </tr>
-            <?php endwhile; ?>
-          </table>
+          </div>
         </div>
       </section>
     </div>
