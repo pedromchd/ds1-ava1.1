391002ab945dbebf3791beb2c277fe3850215eb3
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-05-04 21:45:31 -0300

    oop pdf report

--- report/admin.php
+++ report/admin.php
@@ -10,59 +10,56 @@ if ($user !== 1 || $name !== 'admin') {
 }
 
 $db = new mysqli('localhost', 'root', '', 'library');
-$result = $db->query('SELECT game.*, IFNULL(system.name, "[empty]") AS systemName, user.name AS userName FROM game LEFT JOIN system ON game.system = system.id JOIN user ON game.user = user.id');
+$result = $db->query('SELECT game.id, game.name, game.year, IFNULL(system.name, "[empty]") AS systemName, game.developer, user.name AS userName FROM game LEFT JOIN system ON game.system = system.id JOIN user ON game.user = user.id');
 $db->close();
 
 require_once('../tcpdf/tcpdf.php');
 
-$pdf = new TCPDF('L');
+class MYPDF extends TCPDF {
+  function MultiRow(array $row = []) {
+    $cellWidth = ($this->getPageWidth() - 20) / sizeof($row);
+    $y_start = $y_end = $this->GetY();
+    foreach ($row as $value) {
+      $this->MultiCell($cellWidth, 0, $value, 1, 'L', true, 2, $this->GetX(), $y_start, true, 0);
+      $y_end = max($this->GetY(), $y_end);
+    }
+    $this->setXY(10, $y_end);
+  }
+}
 
+$pdf = new MYPDF('L', 'mm', 'A4', true, 'UTF-8');
+
+$pdf->setCreator(PDF_CREATOR);
+$pdf->setAuthor($name);
 $pdf->setTitle('Game Report');
 
-$pdf->setPrintHeader(false);
-$pdf->setPrintFooter(false);
+$pdf->setHeaderData('', 0, "Users' Game Report", 'by Querty Library', array(0, 0, 0), array(0, 0, 0));
+$pdf->setHeaderFont(array('helvetica', 'B', 16));
+$pdf->setHeaderMargin(10);
+
+$pdf->setFooterData(array(0, 0, 0), array(0, 0, 0));
+$pdf->setFooterFont(array('helvetica', 'R', 10));
+$pdf->setFooterMargin(10);
+
+$pdf->setMargins(10, 30, 10);
+$pdf->setAutoPageBreak(true, 20);
 
 $pdf->AddPage();
 
-$html = <<<EOD
-  <style>
-    table, td {
-      background-color: #dcfce7;
-      border: 1px solid #86efac;
-    }
-    th {
-      background-color: #bbf7d0;
-      border: 1px solid #86efac;
-      font-weight: bold;
-    }
-  </style>
+$pdf->setCellPaddings(2, 2, 2, 2);
+$pdf->setLineStyle(array('width' => 0.25, 'color' => array(74, 222, 128)));
 
-  <h1>users' game report</h1>
-  <table cellpadding="5">
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Year</th>
-      <th>System</th>
-      <th>Developer</th>
-      <th>User</th>
-    </tr>
-EOD;
+$pdf->setFillColorArray(array(134, 239, 172));
+$pdf->setFont('helvetica', 'B', 12);
+
+$pdf->MultiRow(['ID', 'Name', 'Year', 'System', 'Developer', 'User']);
+
+$pdf->setFillColorArray(array(187, 247, 208));
+$pdf->setFont('helvetica', 'R', 12);
 
 while ($row = $result->fetch_assoc()) {
-  $html .= <<<EOD
-    <tr>
-      <td>$row[id]</td>
-      <td>$row[name]</td>
-      <td>$row[year]</td>
-      <td>$row[systemName]</td>
-      <td>$row[developer]</td>
-      <td>$row[userName]</td>
-    </tr>
-  EOD;
+  $pdf->MultiRow($row);
 }
 
-$pdf->writeHTML($html . '</table>');
-
-$pdf->Output('admin.pdf');
+$pdf->Output('admin.pdf', 'I');
 exit;
--- report/summary.php
+++ report/summary.php
@@ -23,3 +23,16 @@ $pdf->setPrintHeader(false);
 $pdf->setPrintFooter(false);
 
 $pdf->AddPage();
+
+$pdf->setCellMargins(1, 1, 1, 1);
+$pdf->setCellPaddings(1, 1, 1, 1);
+
+$pdf->MultiCell(20, 0, 'Hello world!', 1, 'L', 0, 0);
+$pdf->writeHTMLCell(20, 0, $pdf->GetX(), $pdf->GetY(), '<b>Hello!</b>', 1, 0, 0);
+
+$pdf->Ln();
+
+$pdf->MultiCell(20, 0, 'Hello!', 1, 'L', 0, 0);
+
+$pdf->Output('summary.pdf');
+exit;
